d3a018e3e7ca8e3446524b8b1abc2b7ab574f073
Mark Wandera <mmwandera74@gmail.com>
2025-10-30 00:46:39 +0300

    add light rays to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 const page = () => {
   return (
-    <h1>Welcome to the Home Page</h1>
+    <section>
+      <h1 className="text-center">The Hub for Every Dev Event <br />You Mustn&apos;t Miss</h1>
+      <p className="text-center mt-5">Hackatons, Meetups and Conferences All in One Place</p>
+    </section>
   )
 }
 

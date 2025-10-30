7bc246e987b6c21e14606c5db405e23e3daecb1f
Mark Wandera <mmwandera74@gmail.com>
2025-10-30 11:44:45 +0300

    add explore button

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import ExploreBtn from "./components/ExploreBtn"
+
 const page = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev Event <br />You Mustn&apos;t Miss</h1>
       <p className="text-center mt-5">Hackatons, Meetups and Conferences All in One Place</p>
+      <ExploreBtn/>
     </section>
   )
 }
